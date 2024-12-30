4ab542bbad90bb542ea9018850eb28cdbc3499e8
Sohail <sohailkarmani16@gmail.com>
2024-12-30 09:43:52 +0500

    added

---
+++ OptionPaneEx.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class OptionPaneEx {
+    JFrame f;
+    OptionPaneEx(){
+        f=new JFrame();
+        JOptionPane.showMessageDialog(f,"Successfully Updated","Alert",JOptionPane.WARNING_MESSAGE);
+    }
+    public static void main(String[] args) {
+        new OptionPaneEx();
+    }
+}
